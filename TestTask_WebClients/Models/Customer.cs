1b3640742960134b170d32105b7889d0f5412f1d
Rostislav Pecherski <neckazhuvam@gmail.com>
2022-02-13 22:54:25 +0200

    add customer model

--- TestTask_WebClients/Models/Customer.cs
+++ TestTask_WebClients/Models/Customer.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TestTask_WebClients.Models
 {
+    [Table("Customers")]
+    [Index("Emails", IsUnique = true)]
     public class Customer
     {
-        
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        [Column("emails")]
+        public string Emails { get; set; }
+
+        [Column("gender")]
+        public bool Gender { get; set; }
+
+        [Column("day_of_birth")]
+        [Required]
+        public DateTime DayOfBirth { get; set; }
     }
 }
