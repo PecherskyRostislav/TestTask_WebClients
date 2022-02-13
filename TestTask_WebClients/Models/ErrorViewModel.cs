68c20a95e984ce7ee4f704b8525011e7ecb47f0d
Rostislav Pecherski <neckazhuvam@gmail.com>
2022-02-13 20:45:18 +0200

    Add project files.

---
+++ TestTask_WebClients/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestTask_WebClients.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
